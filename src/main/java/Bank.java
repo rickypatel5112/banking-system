0de15df4e477ee3c01ad393513ebe72c728b65be
rickypatel5112 <rickypatel5112@gmail.com>
2023-11-14 23:52:29 -0500

    Simplified the return statement in accountExistsById()

--- src/main/java/Bank.java
+++ src/main/java/Bank.java
@@ -2,43 +2,40 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class Bank {
-	private Map<String, GenericAccount> accounts;
+    private Map<String, GenericAccount> accounts;
 
-	Bank() {
-		accounts = new HashMap<>();
-	}
+    public Bank() {
+        accounts = new HashMap<>();
+    }
 
-	// Returns all the accounts in the bank
-	public Map<String, GenericAccount> getAccounts() {
-		return accounts;
-	}
+    // Returns all the accounts in the bank
+    public Map<String, GenericAccount> getAccounts() {
+        return accounts;
+    }
 
-	// Adds an account to the bank
-	public void addAccount(GenericAccount genericAccount) {
-		accounts.put(genericAccount.getId(), genericAccount);
-	}
+    // Adds an account to the bank
+    public void addAccount(GenericAccount genericAccount) {
+        accounts.put(genericAccount.getId(), genericAccount);
+    }
 
-	// Get account details based on id
-	public GenericAccount getAccount(String id) {
-		return accounts.get(id);
-	}
+    // Get account details based on id
+    public GenericAccount getAccount(String id) {
+        return accounts.get(id);
+    }
 
-	// Deposit money through accountId
-	public void depositMoneyByID(String id, double amount) {
-		accounts.get(id).depositMoney(amount);
-	}
+    // Deposit money through accountId
+    public void depositMoneyByID(String id, double amount) {
+        accounts.get(id).depositMoney(amount);
+    }
 
-	// Withdraw money through accountId
-	public void withdrawMoneyByID(String id, double amount) {
-		accounts.get(id).withdrawMoney(amount);
-	}
+    // Withdraw money through accountId
+    public void withdrawMoneyByID(String id, double amount) {
+        accounts.get(id).withdrawMoney(amount);
+    }
 
-	public boolean accountExistsById(String id){
-		if(accounts.containsKey(id)){
-			return true;
-		}
-		else{
-			return false;
-		}
-	}
+    public boolean accountExistsById(String id) {
+        return accounts.containsKey(id);
+    }
 }
+
+
